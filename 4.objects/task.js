f50e74137ae29a7947961e2bd26d3677169b4c30
Shuralol <akinfeev09@bk.ru>
2022-12-18 14:46:52 +0700

    Исправил четвертое задание

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -13,8 +13,13 @@ Student.prototype.setSubject = function (subjectName) {
   this.subject = subjectName;
 };
 Student.prototype.addMarks = function (...marks) {
-  this.marks === [marks];
-  this.marks.push(...marks);
+  if (this.marks !== undefined) {
+    this.marks.push(...marks);
+  } else {
+    this.marks = [marks];
+  }
+  /* this.marks === [marks]; */
+  /* this.marks.push(...marks); */
 };
 
 Student.prototype.getAverage = function () {
@@ -22,16 +27,14 @@ Student.prototype.getAverage = function () {
     return 0;
   }
   let sum = 0;
-  for (let i = 0; i < this.marks.length; i++) {
-    sum += this.marks.length[i];
+  for (let i = 0; i < this.marks; i++) {
+    sum += this.marks[i];
   }
-  return sum / this.marks.length;
+  return sum / this.marks[i];
 };
 
 Student.prototype.exclude = function (reason) {
-    this.exclude = reason;
-    delete this.subject;
-    delete this.marks;
+  this.excluded = reason;
+  delete this.subject;
+  delete this.marks;
 };
-
-
