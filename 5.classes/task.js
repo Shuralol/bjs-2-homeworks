c907c898db122824ec3a61e40eb5027102cf1d03
Shuralol <akinfeev09@bk.ru>
2022-12-22 12:49:53 +0700

    5 задание задача 1 22.12.22

--- 5.classes/task.js
+++ 5.classes/task.js
@@ -1,11 +1,12 @@
 /* ЗАДАЧА №1*/
 
 class PrintEditionItem {
-  constructor(name, releaseDate, pagesCount, state, type = null) {
+  constructor(name, releaseDate, pagesCount, state) {
     this.name = name;
     this.releaseDate = releaseDate;
     this.pagesCount = pagesCount;
     this.state = 100;
+    this.type = null;
   }
   fix() {
     this.state *= 1.5;
@@ -26,8 +27,8 @@ class PrintEditionItem {
 }
 
 class Magazine extends PrintEditionItem {
-  constructor(name, releaseDate, pagesCount, state, type) {
-    super(name, releaseDate, pagesCount, state, type);
+  constructor(name, releaseDate, pagesCount) {
+    super(name, releaseDate, pagesCount);
     this.type = "magazine";
   }
 }
@@ -41,22 +42,22 @@ class Book extends PrintEditionItem {
 }
 
 class NovelBook extends Book {
-  constructor(name, releaseDate, pagesCount,) {
-    super(name, releaseDate, pagesCount);
+  constructor(author, name, releaseDate, pagesCount) {
+    super(author, name, releaseDate, pagesCount);
     this.type = "novel";
   }
 }
 
 class FantasticBook extends Book {
-  constructor(name, releaseDate, pagesCount) {
-    super(name, releaseDate, pagesCount);
+  constructor(author, name, releaseDate, pagesCount) {
+    super(author, name, releaseDate, pagesCount);
     this.type = "fantastic";
   }
 }
 
 class DetectiveBook extends Book {
-  constructor(name, releaseDate, pagesCount) {
-    super(name, releaseDate, pagesCount);
+  constructor(author, name, releaseDate, pagesCount) {
+    super(author, name, releaseDate, pagesCount);
     this.type = "detective";
   }
 }
