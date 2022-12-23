60856dbd3948bae1cefa0e82d8495eb23589c788
Shuralol <akinfeev09@bk.ru>
2022-12-23 17:26:09 +0700

    6 задание 1 задача

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,23 @@
-﻿
+﻿/* Задача 1 */
+
+function parseCount(parse) {
+  return Number.parseFloat(parse);
+}
+console.log(Number.parseFloat("12.3px"));
+
+if (Number.parseFloat("FF2")) {
+  throw new Error("невалидное значение");
+}
+
+function validateCount(parse) {
+  try {
+    const parseNumber = Number.parseFloat(parse);
+    if (!isFinite(parseNumber)) {
+      throw new Error("не валидное значение");
+    }
+    return parseNumber;
+  } catch (error) {
+    return error.message;
+  }
+}
+validateCount(5.33);
